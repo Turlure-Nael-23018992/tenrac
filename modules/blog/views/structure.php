8e58c9ab89dd4d94b60f19b96cbc138cd2615e23
MEHDI-HAMDI-Yousra-2326076aa <yousra.MEHDI-HAMDI@etu.univ-amu.fr>
2024-09-19 15:21:49 +0200

    Avancement sur les controlleurs

--- modules/blog/views/structure.php
+++ modules/blog/views/structure.php
@@ -21,3 +21,7 @@ include 'header.php';
 </main>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<?php
+        }
+}
+?>
